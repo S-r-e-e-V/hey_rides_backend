4fa8c29260cb24b06550fa1ff49bafd523c9556b
Sreekanth V <25sreekanth@gmail.com>
2024-01-05 01:14:32 -0500

    fix: mail issue

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -1,19 +1,28 @@
 const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
 
+const auth = {
+  type: "oauth2",
+  user: "heyrides06@gmail.com",
+  clientId:
+    "344515196022-pct3rhfjv2eharesnt2493udv802bncq.apps.googleusercontent.com",
+  clientSecret: "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",
+  refreshToken:
+    "1//04A4I2bQ7tZc7CgYIARAAGAQSNwF-L9IrDk3aQtWwf5P_2Ibypav6gsU6i4QEiu9aAzkJHpkKms0mnAhotjOvpFiA7BmZLSgCU-U",
+  // accessToken: access_token,
+};
 const sendMail = (mailOptions) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
-    auth: {
-      type: "oauth2",
-      user: "heyrides06@gmail.com",
-      clientId:
-        "344515196022-pct3rhfjv2eharesnt2493udv802bncq.apps.googleusercontent.com",
-      clientSecret: "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",
-      refreshToken:
-        "1//04vA1WP4ONodBCgYIARAAGAQSNwF-L9IrRtlHCTYg_LuDRvVO07w_C0J1p_I5vrImW2PdwrRM8kXFCbvbRVNILCbWi3Q67_QK2e4",
-      // accessToken: access_token,
-    },
+    auth: auth,
   });
+
+  const AuthClient = new google.auth.OAuth2(
+    auth.clientId,
+    auth.clientSecret,
+    auth.refreshToken
+  );
+  AuthClient.setCredentials({ refresh_token: auth.refreshToken });
 
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
